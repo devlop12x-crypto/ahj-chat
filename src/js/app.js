f931a5b92116cdeb065302bf845012ddf514d164
devlop12x-crypto <devlop12x@gmail.com>
2026-07-13 19:28:37 +0300

    Update SERVER_URL to new backend address

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ import ChatAPI from './api/ChatAPI';
 
 // URL серверной части, развёрнутой на Render.
 // После деплоя бэкенда замените на адрес своего сервиса.
-const SERVER_URL = 'https://ahj-chat-backend.onrender.com';
+const SERVER_URL = 'https://ahj-chat-backend-0cc2.onrender.com';
 
 const api = new ChatAPI(SERVER_URL);
 const chat = new Chat(document.querySelector('#root'), api);
